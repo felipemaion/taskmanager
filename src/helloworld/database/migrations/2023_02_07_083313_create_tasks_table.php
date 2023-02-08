5860431ed574a97db740df40d6121575389df1d0
Felipe Maion <felipe.maion@gmail.com>
2023-02-08 02:29:40 -0300

    Initial Commit

---
+++ src/helloworld/database/migrations/2023_02_07_083313_create_tasks_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+
+        Schema::create( 'tasks', function( Blueprint $table ) {
+            $table->id();
+            $table->string( 'title', 512 );
+            $table->string( 'description', 1024 )->nullable();
+            $table->smallInteger( 'status' );
+            $table->smallInteger( 'hoursRequired' )->default( 0 );
+            $table->smallInteger( 'hoursConsumed' )->default( 0 );
+            $table->dateTime( 'createdAt' );
+            $table->dateTime( 'updatedAt' )->nullable();
+            $table->date( 'plannedStartDate' )->nullable();
+            $table->date( 'plannedEndDate' )->nullable();
+            $table->date( 'actualStartDate' )->nullable();
+            $table->date( 'actualEndDate' )->nullable();
+            $table->text( 'content' )->nullable();
+            $table->foreignId( 'userId' )->nullable()->constrained( 'users' )->onUpdate( 'cascade' )->onDelete( 'cascade' );
+            $table->foreignId( 'createdBy' )->nullable()->constrained( 'users' )->onUpdate( 'cascade' )->onDelete( 'set null' );
+            $table->foreignId( 'updatedBy' )->nullable()->constrained( 'users' )->onUpdate( 'cascade' )->onDelete( 'set null' );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
